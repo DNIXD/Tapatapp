674e527253ff5f1da402153a7b5cba5f367d6d66
DNIXD <denispapanatas@gmail.com>
2025-01-28 14:47:58 +0100

    6.7

--- Server_prueba_usuarios.py
+++ Server_prueba_usuarios.py
@@ -8,16 +8,25 @@ class User:
         self.username = username
         self.password = password
         self.email = email
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "password": self.password,
+            "email": self.email
+        }
     
     def __str__(self):
         return "User:" + self.username + " pass:" + self.password + " email:" + self.email
 
+
 class DAOUsers:
-    def _init_(self):
-        self.users=listUsers
+    def __init__(self):
+        self.users = listUsers
 
     def getUserByUsername(self, username):
-        for u in listUsers:
+        for u in self.users:
             if username == u.username:
                 return u
         return None
@@ -28,33 +37,32 @@ listUsers = [
     User(id=2, username="usuari2", password="54321", email="usuari2@gmail.com"),
     User(id=3, username="usuari3", password="54321", email="usuari3@gmail.com"),
     User(id=4, username="usuari4", password="67890", email="usuari4@gmail.com")
-    ]
+]
+
 for u in listUsers:
     print(u)
 
 daousers = DAOUsers()
 
-print(daousers.getUserByUsername("usuari4"))
-u=daousers.getUserByUsername("notrobat")
-
-if(u):
-    print(u)
-else:
-    print("No encontrad")
-
 @app.route('/hello', methods=['GET'])
 def hello():
-    return "Hello World"
+    user = daousers.getUserByUsername("usuari1")
+    if user:
+        return jsonify(user.to_dict())  # Serializa el objeto a un diccionario
+    return jsonify({"error": "User not found"}), 404
+
 
 @app.route('/tapatapp/getuser', methods=['GET'])
 def getuser():
-    n= str(request.args.get('name'))
-    email= str(request.args.get('mail'))
+    n = str(request.args.get('name'))
+    email = str(request.args.get('mail'))
     return "HelloWorld Name = " + n + " Mail = " + email
+
 
 @app.route('/prototip/getuser/<string:username>', methods=['GET'])
 def prototip(username):
     return "Prototip1 1, user: " + username
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="10050")
