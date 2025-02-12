20970ac457c3c66beb3eb68a6568c9fa97d5e22d
DNIXD <denispapanatas@gmail.com>
2025-02-12 14:01:05 +0100

    7.7

--- Server_prototipo2.py
+++ Server_prototipo2.py
@@ -11,13 +11,21 @@ class DAOUsers:
 
 class DAOChilds:
     def __init__(self):
-        self.users = dades.children
+        self.children = dades.children  # Lista de niños
 
-    def getChildbyUser_ID(self, id):
-        for u in self.users:
-            if id == u.id:
-                for i in dades.relation_user_child{
-                    if i[1] = 1:
-                        child_id = i[2]
-                }
-        return None
+    def getChildbyUser_ID(self, user_id):
+        child_ids = [rel["child_id"] for rel in dades.relation_user_child if rel["user_id"] == user_id]
+
+        if not child_ids: 
+            return None
+
+        children_info = [child for child in self.children if child.id in child_ids] 
+        return children_info
+
+class DAOTaps:
+    def __init__(self):
+        self.taps = dades.taps
+
+    def getTapByChild_ID(self, child_id):
+        taps_info = [tap for tap in self.taps if tap.child_id == child_id]
+        return taps_info if taps_info else None
